c42b5ee5ea57b5dd4430ed5c2be6c87c3deed311
Jit <jit@example.com>
2025-04-30 11:32:01 +0100

    Code fix

--- src/index.js
+++ src/index.js
@@ -15,7 +15,3 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
-function Dummy(input){
-  var sql = "SELECT * From Customers WHERE name='" + input + "';"
-}
